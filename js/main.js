79e699da31f802843c84bae04447df0fad3f9603
Zach Thacker <zpthacker@gmail.com>
2015-06-23 21:00:31 -0400

    Add autocomplete comboboxes instead of regular selects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// Application JavaScript
+var getSelectCsv = function(selector, url) {
+    $.get(url, function(data) {
+        var options = [];
+        data.trim().split('\n').forEach(function(row) {
+            var values = row.split('#');
+            $(selector).append(
+                '<option value="' + values[0] + '">' + values[1] + '</option>'
+            );
+        });
+        $(selector).combobox();
+    });
+};
+getSelectCsv('#product-problem', 'data/product_problem.data');
+getSelectCsv('#product-class', 'data/product_class.data');
+getSelectCsv('#event-type', 'data/event_type.data');
